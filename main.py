2176d09548d27641864f0f621760998bce004d57
Mariusz Drabik <drabik.mariusz@gmail.com>
2021-10-30 19:04:47 +0200

    first rebase

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from datetime import datetime, timedelta, time
+from datetime import datetime
 
 
 if __name__ == '__main__':
+
     print("jestesmy na branchu tinker views")
+
+    date = datetime.now()
+    print(date)
